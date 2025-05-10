d6fb05204c03047e23e774cc6fe55a79b45b4301
Vkanhan <vishnukanhandev@gmail.com>
2025-05-10 09:41:27 +0000

    minor fix

--- config/config.go
+++ config/config.go
@@ -1,41 +1,38 @@
 package config
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	ServerPort  string
-	DBHost      string
-	DBPort      string
-	DBUser      string
-	DBPassword  string
-	DBName      string
-	DBSSLMode   string
+	ServerPort string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBSSLMode  string
 }
 
 func Load() (*Config, error) {
-	if err := godotenv.Load(); err != nil {
-		fmt.Println("Warning: .env file not found. Using environment variables.")
-	}
+	godotenv.Load("../../.env") 
 
 	return &Config{
-		ServerPort:  getEnv("SERVER_PORT"),
-		DBHost:      getEnv("DB_HOST"),
-		DBPort:      getEnv("DB_PORT"),
-		DBUser:      getEnv("DB_USER"),
-		DBPassword:  getEnv("DB_PASSWORD"),
-		DBName:      getEnv("DB_NAME"),
-		DBSSLMode:   getEnv("DB_SSL_MODE"),
+		ServerPort: getEnv("SERVER_PORT"),
+		DBHost:     getEnv("DB_HOST"),
+		DBPort:     getEnv("DB_PORT"),
+		DBUser:     getEnv("DB_USER"),
+		DBPassword: getEnv("DB_PASSWORD"),
+		DBName:     getEnv("DB_NAME"),
+		DBSSLMode:  getEnv("DB_SSL_MODE"),
 	}, nil
 }
 
-func getEnv(key string) (string) {
+func getEnv(key string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
 	}
-	return "" 
+	return ""
 }
